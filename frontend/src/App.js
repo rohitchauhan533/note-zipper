064b0bf7600227535c2376fb443ef5162de9f3d1
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-10-09 20:21:35 +0530

    header and footer setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => (
+  <>
+  <Header />
+  <main style={{minHeight: "93vh"}}></main>
+  <Footer />
+  </>
+)
 
 export default App;
